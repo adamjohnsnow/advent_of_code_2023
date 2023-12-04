395f01fdf3a06a55e033add2f9c0e5c0e02efba2
Adam Snow <adamjohnsnow@icloud.com>
2023-12-04 10:17:02 +0000

    I'm going to come back to this

--- puzzle_3/puzzle_3.rb
+++ puzzle_3/puzzle_3.rb
@@ -1,4 +1,4 @@
-@file = File.readlines('./puzzle_input_3.txt', chomp: true)
+@file = File.readlines('./puzzle_input_3_test.txt', chomp: true)
 
 def parse_line(line)
   numbers = []
@@ -9,7 +9,16 @@ def parse_line(line)
   numbers
 end
 
-def check_part(index_y, index_x, length)
+def parse_symbols(line)
+  symbols = []
+  line.scan(/[^.a-zA-Z0-9\s]/) do |match|
+    symbols << { symbol: match, index: Regexp.last_match.begin(0) }
+  end
+  
+  symbols
+end
+
+def get_neighbours(index_y, index_x, length)
   neighbours = []
   left_edge = index_x == 0 ? 0 : index_x -1
   
@@ -18,12 +27,28 @@ def check_part(index_y, index_x, length)
   neighbours << @file[index_y][index_x + length] if index_x + length < @file[index_y].length
   neighbours << @file[index_y + 1][left_edge .. index_x + length] if @file.length > index_y + 1
 
+  neighbours
+end
+
+def check_part(index_y, index_x, length)
+  neighbours = get_neighbours(index_y, index_x, length)
   !!neighbours.join()[/[^.a-zA-Z0-9\s]/]
+end
+
+def check_gear(index_y, index_x)
+  p index_y, index_x
+  p @file.fetch(index_y - 1, '')[index_x - 3..index_x + 3]
 end
 
 sum = 0
 
 @file.each_with_index do |line, index|
+  line << ['...']
+  results = parse_symbols(line)
+  results.each do |result|
+    check_gear(index, result[:index])
+  end
+
   results = parse_line(line)
   results.each do |result|
     sum += result[:number].to_i if check_part(index, result[:index], result[:number].length)
