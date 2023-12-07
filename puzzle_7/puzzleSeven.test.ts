727ed7348f158647c066c9c039dd17e1b12b367f
Adam Snow <adamjohnsnow@icloud.com>
2023-12-07 13:40:00 +0000

    RTDQ

--- puzzle_7/puzzleSeven.test.ts
+++ puzzle_7/puzzleSeven.test.ts
@@ -27,81 +27,35 @@ describe("Puzzle 7", () => {
     ]);
   });
 
-  test("evaluate hand", () => {
-    let hand = [{ value: 14, count: 5 }];
-    expect(evaluateHand(hand)).toEqual({
-      fives: 14,
-      fours: 0,
-      ones: [],
-      threes: 0,
-      twos: [],
-    });
-
-    hand = [{ value: 1, count: 5 }];
-    expect(evaluateHand(hand)).toEqual({
-      fives: 1,
-      fours: 0,
-      ones: [],
-      threes: 0,
-      twos: [],
-    });
-
-    hand = [
-      { value: 1, count: 4 },
-      { value: 13, count: 1 },
-    ];
-    expect(evaluateHand(hand)).toEqual({
-      fives: 0,
-      fours: 1,
-      ones: [13],
-      threes: 0,
-      twos: [],
-    });
-
-    hand = [
-      { value: 2, count: 4 },
-      { value: 13, count: 1 },
-    ];
-    expect(evaluateHand(hand)).toEqual({
-      fives: 0,
-      fours: 2,
-      ones: [13],
-      threes: 0,
-      twos: [],
-    });
-
-    hand = [
-      { value: 1, count: 3 },
-      { value: 13, count: 2 },
-    ];
-    expect(evaluateHand(hand)).toEqual({
-      fives: 0,
-      fours: 0,
-      ones: [],
-      threes: 1,
-      twos: [13],
-    });
-  });
-
   test("ranker", () => {
-    let hand = {
-      fives: 0,
-      fours: 0,
-      ones: [],
-      threes: 1,
-      twos: [13],
-    };
+    let hand = [{ value: 14, count: 5 }];
 
-    expect(ranker(hand).rank).toBe(5);
+    expect(ranker(hand)).toBe(7);
 
-    hand = {
-      fives: 12,
-      fours: 0,
-      ones: [],
-      threes: 0,
-      twos: [],
-    };
+    hand = [
+      { value: 1, count: 1 },
+      { value: 2, count: 1 },
+      { value: 3, count: 1 },
+      { value: 4, count: 2 },
+    ];
 
-    expect(ranker(hand).rank).toBe(7);
+    expect(ranker(hand)).toBe(2);
+
+    hand = [
+      { value: 1, count: 1 },
+      { value: 2, count: 1 },
+      { value: 3, count: 1 },
+      { value: 4, count: 1 },
+      { value: 4, count: 1 },
+    ];
+
+    expect(ranker(hand)).toBe(1);
+
+    hand = [
+      { value: 13, count: 2 },
+      { value: 5, count: 3 },
+    ];
+
+    expect(ranker(hand)).toBe(5);
   });
 });
--- puzzle_7/puzzleSeven.ts
+++ puzzle_7/puzzleSeven.ts
@@ -1,12 +1,4 @@
 const faceCards = ["T", "J", "Q", "K", "A"];
-type Result = {
-  fives: number;
-  fours: number;
-  threes: number;
-  twos: number[];
-  ones: number[];
-  rank?: number;
-};
 
 type Output = {
   value: number;
@@ -35,44 +27,42 @@ export function countKinds(cards: string) {
 }
 
 export function evaluateHand(hand: Output[]) {
-  const result: Result = {
-    fives: hand.find((suit) => suit.count == 5)?.value || 0,
-    fours: hand.find((suit) => suit.count == 4)?.value || 0,
-    threes: hand.find((suit) => suit.count == 3)?.value || 0,
-    twos: hand.filter((suit) => suit.count == 2)?.map((match) => match.value),
-    ones: hand.filter((suit) => suit.count == 1)?.map((match) => match.value),
-  };
-
-  return result;
+  // const result: Result = {
+  //   fives: hand.find((suit) => suit.count == 5)?.value || 0,
+  //   fours: hand.find((suit) => suit.count == 4)?.value || 0,
+  //   threes: hand.find((suit) => suit.count == 3)?.value || 0,
+  //   twos: hand.filter((suit) => suit.count == 2)?.map((match) => match.value),
+  //   ones: hand.filter((suit) => suit.count == 1)?.map((match) => match.value),
+  // };
+  // return result;
 }
 
-export function ranker(result: Result) {
-  if (result.fives > 0) {
-    result.rank = 7;
-    return result;
+export function ranker(result: Output[]) {
+  if (result.find((suit) => suit.count == 5)?.value) {
+    return 7;
   }
-  if (result.fours > 0) {
-    result.rank = 6;
-    return result;
+  if (result.find((suit) => suit.count == 4)?.value) {
+    return 6;
   }
-  if (result.threes > 0 && result.twos.length > 0) {
-    result.rank = 5;
-    return result;
+
+  const threes = result.find((suit) => suit.count == 3)?.value;
+  const twos = result
+    .filter((suit) => suit.count == 2)
+    ?.map((match) => match.value);
+
+  if (threes && twos.length > 0) {
+    return 5;
   }
-  if (result.threes > 0) {
-    result.rank = 4;
-    return result;
+  if (threes) {
+    return 4;
   }
-  if (result.twos.length == 2) {
-    result.rank = 3;
-    return result;
+  if (twos.length == 2) {
+    return 3;
   }
-  if (result.twos.length == 1) {
-    result.rank = 2;
-    return result;
+  if (twos.length == 1) {
+    return 2;
   }
-  result.rank = 1;
-  return result;
+  return 1;
 }
 
 export function superSort(hands: Output[]) {}
