f3b2d13e0dfa8178b5ab9d8cd427909ee8ec17a3
Adam Snow <adamjohnsnow@icloud.com>
2023-12-09 12:48:09 +0000

    D9P1 done

--- puzzle_9/puzzleNine.test.ts
+++ puzzle_9/puzzleNine.test.ts
@@ -1,10 +1,107 @@
 import { describe, expect, test } from "@jest/globals";
-import { convertLine } from "./puzzleNine";
+import {
+  convertLine,
+  findNext,
+  findPrevious,
+  isSame,
+  processLine,
+} from "./puzzleNine";
+import * as fs from "fs";
 
 describe("puzzle nine", () => {
   test("convert a line", () => {
     const line = [0, 3, 6, 9];
 
     expect(convertLine(line)).toEqual([3, 3, 3]);
+  });
+
+  test("repeat checker", () => {
+    let line = [3, 3, 3, 3];
+    expect(isSame(line)).toBe(true);
+    line = [3, 2, 3, 3];
+    expect(isSame(line)).toBe(false);
+  });
+
+  test("line processor", () => {
+    let line = "0 3 6 9";
+    let processed = processLine(line);
+
+    expect(processed[0][0]).toEqual(0);
+    expect(processed[0][2]).toEqual(6);
+    expect(processed[1]).toEqual([3, 3, 3]);
+
+    line = "10 13 16 21 30 45";
+    processed = processLine(line);
+
+    expect(processed[0][0]).toEqual(10);
+  });
+
+  test("find next number", () => {
+    let line = "0 3 6 9 12";
+    let processed = processLine(line);
+
+    expect(findNext(processed)).toEqual(15);
+
+    line = "10 13 16 21 30 45";
+    processed = processLine(line);
+
+    expect(findNext(processed)).toEqual(68);
+  });
+
+  test("test run - next", () => {
+    let answers: number[] = [];
+    let sum = 0;
+    fs.readFile("puzzle_9/test.txt", "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading the file:", err);
+        return 0;
+      }
+      const lines: string[] = data.split("\n");
+      lines.forEach((line) => {
+        const processed = processLine(line);
+        const result = findNext(processed);
+        sum += result;
+        answers.push(result);
+      });
+      expect(sum).toEqual(114);
+    });
+  });
+
+  test("big run - next", () => {
+    let answers: number[] = [];
+    let sum = 0;
+    fs.readFile("puzzle_9/input.txt", "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading the file:", err);
+        return 0;
+      }
+      const lines: string[] = data.split("\n");
+      lines.forEach((line) => {
+        const processed = processLine(line);
+        const result = findNext(processed);
+        sum += result;
+        answers.push(result);
+      });
+      expect(sum).toEqual(2101499000);
+    });
+  });
+
+  test("test run - prev", () => {
+    let answers: number[] = [];
+    let sum = 0;
+    fs.readFile("puzzle_9/test.txt", "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading the file:", err);
+        return 0;
+      }
+      const lines: string[] = data.split("\n");
+      lines.forEach((line) => {
+        const processed = processLine(line);
+        const result = findPrevious(processed);
+        sum += result;
+        answers.push(result);
+      });
+      expect(sum).toEqual(114);
+    });
   });
 });
--- puzzle_9/puzzleNine.ts
+++ puzzle_9/puzzleNine.ts
@@ -1,7 +1,52 @@
-export function convertLine(line: number[]) {
+export function convertLine(line: number[]): number[] {
   const newLine: number[] = [];
   for (let i = 0; i < line.length - 1; i++) {
     newLine.push(line[i + 1] - line[i]);
   }
   return newLine;
+}
+
+export function isSame(line: number[]): boolean {
+  const targetNumber = line[0];
+  const repeats = line.reduce(
+    (count, number) => (number === targetNumber ? count + 1 : count),
+    0
+  );
+  return line.length === repeats;
+}
+
+export function processLine(line: string): number[][] {
+  let lines: number[][] = [];
+  let newLine: number[] = [];
+  line.split(" ").forEach((num) => {
+    newLine.push(parseInt(num, 10));
+  });
+  lines.push(newLine);
+
+  while (!isSame(newLine)) {
+    newLine = convertLine(newLine);
+    lines.push(newLine);
+  }
+  return lines;
+}
+
+export function findNext(input: number[][]): number {
+  let factor: number = input[input.length - 1][0];
+  for (let i = input.length - 2; i > 0; i--) {
+    const last = input[i][input[i].length - 1];
+
+    factor = last + factor;
+  }
+  const result = input[0][input[0].length - 1] + factor;
+  return result;
+}
+export function findPrevious(input: number[][]): number {
+  let factor: number = input[input.length - 1][0];
+  for (let i = input.length - 2; i > 0; i--) {
+    const last = input[i][input[i].length - 1];
+
+    factor = last + factor;
+  }
+  const result = input[0][input[0].length - 1] + factor;
+  return result;
 }
