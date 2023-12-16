9a875c318507c2adea38d48df5e0f018593d416f
Adam Snow <adamjohnsnow@icloud.com>
2023-12-16 13:51:01 +0000

    shouldn't have done ruby

--- puzzle_14/puzzle_14.rb
+++ puzzle_14/puzzle_14.rb
@@ -1,26 +1,49 @@
-file_lines = File.readlines('input.txt', chomp: true)
+dish = File.readlines('test.txt', chomp: true)
 
-tilt_north = file_lines.map(&:chars).transpose.map(&:join)
-load = 0
-tilt_north.each do |line|
-  space = nil
-  max_load = line.length
-  line.split('').each_with_index do |slot, index|
-    space = index if !space && slot == "."
-    space = nil if slot == "#"
-    if slot == "O" && !space.nil?
-      line[space] = "O"
-      line[index] = "."
-      load += max_load - space
-      space += 1
+@load = 0
+
+def let_rocks_slide(dish)
+  dish.each do |line|
+    space = nil
+    max_load = line.length
+    line.split('').each_with_index do |slot, index|
+      space = index if !space && slot == "."
+      space = nil if slot == "#"
+      if slot == "O" && !space.nil?
+        line[space] = "O"
+        line[index] = "."
+        space += 1
+      end
     end
-
-    if slot == "O" && !space
-      load += max_load - index
-    end
-
   end
 end
 
-p load
-p tilt_north.map(&:chars).transpose.map(&:join)
+def calc_load(dish)
+  load = 0
+  dish.each do |col|
+    col.split('').each_with_index do |item, index|
+      if item == "O"
+        load += col.length - index
+      end
+    end
+  end
+  load
+end
+
+tilt_north = dish.map(&:chars).transpose.map(&:join)
+dish = let_rocks_slide(tilt_north)
+cycle = 1
+resluts = []
+1000000000.times do
+  cycle+=1
+  dish = let_rocks_slide(dish.map(&:chars).transpose.map(&:join))
+  dish = let_rocks_slide(dish.map(&:chars).reverse.transpose.map(&:join))
+  dish = let_rocks_slide(dish.map(&:chars).reverse.transpose.map(&:join))
+  dish = dish.map(&:chars).reverse.transpose.map(&:join).reverse
+
+  @load = calc_load(dish)
+  dish = let_rocks_slide(dish)
+  p cycle
+end
+
+p @load
